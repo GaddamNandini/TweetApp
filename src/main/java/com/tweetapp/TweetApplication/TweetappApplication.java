b04585ed454f107408dcaf6c556aa7d43ebf931c
unknown <cogjava4350@iiht.tech>
2022-11-11 10:37:18 +0530

    tweetapplication

---
+++ src/main/java/com/tweetapp/TweetApplication/TweetappApplication.java
@@ -0,0 +1,13 @@
+package com.tweetapp.TweetApplication;
+
+import com.tweetapp.TweetApplication.MainMenu;
+
+public class TweetappApplication {
+
+	public static void main(String[] args) {
+		MainMenu controller = new MainMenu();
+		controller.beforeLogin();
+
+	}
+
+}
